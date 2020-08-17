f3efd661a091dcc910856839aa8e847531b9503d
Andres Quintero <andres.quintero98@hotmail.com>
2020-08-17 16:52:02 -0500

    Part I y II

---
+++ src/Todo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export class Todo extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+                //Add your code here to represent a TODO
+                <div>
+                    <h2>Text: {this.props.text}</h2>
+                    <h2>Priority: {this.props.priority}</h2>
+                    <h2>Due Date: {this.props.dueDate.toString()}</h2>
+                </div>
+                );
+    }
+
+}
